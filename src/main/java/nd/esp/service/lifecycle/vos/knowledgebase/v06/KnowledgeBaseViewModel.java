5b38c69f8a61b9d7e71c3668733cd28fc554b11d
cumtxzy <xuzhenyu.0929@163.com>
2016-05-03 19:19:48 +0800

    知识库查询增加description/creator/createTime返回

--- src/main/java/nd/esp/service/lifecycle/vos/knowledgebase/v06/KnowledgeBaseViewModel.java
+++ src/main/java/nd/esp/service/lifecycle/vos/knowledgebase/v06/KnowledgeBaseViewModel.java
@@ -1,10 +1,15 @@
 package nd.esp.service.lifecycle.vos.knowledgebase.v06;
 
+import java.util.Date;
+
 public class KnowledgeBaseViewModel {
 	public String identifier;
 	public String kpid;
 	public String knid;
 	public String title;
+	public String description;
+	public String creator;
+	public Date createTime;
 	public String getIdentifier() {
 		return identifier;
 	}
@@ -28,6 +33,24 @@ public class KnowledgeBaseViewModel {
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
 	}
 	
 }
