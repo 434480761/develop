9f5618aec705d68f7092a94e5bf1c070b199416d
cumtxzy <xuzhenyu.0929@163.com>
2016-05-04 17:20:01 +0800

    智慧知识中的知识点查询支持description模型查询

--- src/main/java/nd/esp/service/lifecycle/daos/knowledgebase/v06/KnowledgeBaseDaoImpl.java
+++ src/main/java/nd/esp/service/lifecycle/daos/knowledgebase/v06/KnowledgeBaseDaoImpl.java
@@ -73,7 +73,7 @@ public class KnowledgeBaseDaoImpl implements KnowledgeBaseDao {
 		List<KnowledgeBaseModel> returnList = new ArrayList<KnowledgeBaseModel>();
 		knTitle = "%"+knTitle+"%";
 		if(StringUtils.isNotEmpty(knTitle)){
-			String sql = "SELECT kb.identifier,kb.knid,kb.kpid,nd.title,nd.description,nd.creator,nd.create_time FROM knowledge_base kb,ndresource nd where kb.kpid = :kpid and nd.primary_category = 'knowledges' and nd.enable = 1 and kb.knid = nd.identifier and nd.title like :knTitle";
+			String sql = "SELECT kb.identifier,kb.knid,kb.kpid,nd.title,nd.description,nd.creator,nd.create_time FROM knowledge_base kb,ndresource nd where kb.kpid = :kpid and nd.primary_category = 'knowledges' and nd.enable = 1 and kb.knid = nd.identifier and (nd.title like :knTitle or nd.description like :knTitle)";
 			Query query = em.createNativeQuery(sql);
 			query.setParameter("kpid", kpid);
 			query.setParameter("knTitle", knTitle);
