67a8f18ae53526616b096336aa49e502a9c77d7b
xiezy <xiezy@101.com>
2016-05-12 14:37:39 +0800

    Revert "es version upadte to 2.3.1"
    This reverts commit 88af4e2efee05b7ac9368acc9560ca085af68f35.

--- src/main/java/nd/esp/service/lifecycle/support/busi/elasticsearch/EsClientSupport.java
+++ src/main/java/nd/esp/service/lifecycle/support/busi/elasticsearch/EsClientSupport.java
@@ -1,18 +1,14 @@
 package nd.esp.service.lifecycle.support.busi.elasticsearch;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import nd.esp.service.lifecycle.support.Constant;
-import nd.esp.service.lifecycle.support.LifeCircleException;
 import nd.esp.service.lifecycle.support.enums.ElasticSearchReservedWords;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
 
 /**
  * 用于管理elasticsearch 客户端
@@ -21,31 +17,23 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class EsClientSupport {
-	private static final Logger logger = LoggerFactory
-			.getLogger(EsClientSupport.class);
 
 	/**
 	 * 获取elasticsearch 客户端（新建）
 	 * 
 	 * @return
-	 * @throws UnknownHostException 
-	 * @throws NumberFormatException 
 	 */
-	public static Client getClient(){
-		Settings settgins = Settings
+	public static Client getClient() {
+		Settings settgins = ImmutableSettings
 				.builder()
 				.put(ElasticSearchReservedWords.CLUSTER_NAME.toString(),
 						Constant.ES_CLUSTER_NAME)
 				.put(ElasticSearchReservedWords.CLIENT_TRANSPORT_SNIFF.toString(), true)
 				/* .put(EsExecutors.PROCESSORS, 100) */.build();
-		TransportClient client = TransportClient.builder().settings(settgins).build();
-		try {
-			return client.addTransportAddress(new InetSocketTransportAddress(
-					InetAddress.getByName(Constant.ES_DOMAIN), Integer.valueOf(Constant.ES_PORT)));
-		} catch (NumberFormatException | UnknownHostException e) {
-			logger.error(e.getMessage());
-			throw new LifeCircleException(e.getMessage());
-		}
+		@SuppressWarnings("resource")
+		TransportClient client = new TransportClient(settgins);
+		return client.addTransportAddress(new InetSocketTransportAddress(
+				Constant.ES_DOMAIN, Integer.valueOf(Constant.ES_PORT)));
 
 	}
 
