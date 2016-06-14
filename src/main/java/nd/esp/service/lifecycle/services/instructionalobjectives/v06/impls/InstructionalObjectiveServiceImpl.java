38d0660fa1417b488c560a074da4dd84479e5cf1
cumtxzy <xuzhenyu.0929@163.com>
2016-06-14 21:27:34 +0800

    1、教学目标资源的修改存放多个历史“版本” 2、关系接口暂时去除order_num校验

--- src/main/java/nd/esp/service/lifecycle/services/instructionalobjectives/v06/impls/InstructionalObjectiveServiceImpl.java
+++ src/main/java/nd/esp/service/lifecycle/services/instructionalobjectives/v06/impls/InstructionalObjectiveServiceImpl.java
@@ -1,10 +1,22 @@
 package nd.esp.service.lifecycle.services.instructionalobjectives.v06.impls;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import nd.esp.service.lifecycle.educommon.services.NDResourceService;
+import nd.esp.service.lifecycle.models.v06.EducationRelationLifeCycleModel;
+import nd.esp.service.lifecycle.models.v06.EducationRelationModel;
 import nd.esp.service.lifecycle.models.v06.InstructionalObjectiveModel;
+import nd.esp.service.lifecycle.services.educationrelation.v06.EducationRelationServiceV06;
 import nd.esp.service.lifecycle.services.instructionalobjectives.v06.InstructionalObjectiveService;
 import nd.esp.service.lifecycle.support.enums.ResourceNdCode;
+import nd.esp.service.lifecycle.utils.CollectionUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +31,13 @@ public class InstructionalObjectiveServiceImpl implements InstructionalObjective
 
     @Autowired
     private NDResourceService ndResourceService;
+    
+    @Autowired
+    @Qualifier("educationRelationServiceV06")
+    private EducationRelationServiceV06 educationRelationService;
+    
+    @Autowired
+    private JdbcTemplate jt;
 
     @Override
     public InstructionalObjectiveModel createInstructionalObjective(InstructionalObjectiveModel instructionalObjectiveModel) {
@@ -34,14 +53,74 @@ public class InstructionalObjectiveServiceImpl implements InstructionalObjective
 
     @Override
     public InstructionalObjectiveModel updateInstructionalObjective(InstructionalObjectiveModel instructionalObjectiveModel) {
-        // 调用通用创建接口
-        instructionalObjectiveModel.setTechInfoList(null);
-        instructionalObjectiveModel = (InstructionalObjectiveModel) ndResourceService.update(ResourceNdCode.instructionalobjectives.toString(),
-                                                                                             instructionalObjectiveModel);
-        instructionalObjectiveModel.setPreview(null);
-        instructionalObjectiveModel.setEducationInfo(null);
+    	/**
+    	 *由于教学目标的演示demo需要有版本的信息
+    	 *目前采用的做法
+    	 *修改教学目标时，如果状态不是为ONLINE，则新增一条教学目标，并与之前的教学目标（有可能需要通过关系查询最原始的教学目标）建立关系
+    	 *修改教学目标时，如果状态为ONLINE，则需要找到最原始的教学目标，并将新的内容赋值给原始的教学目标
+    	 */
 
-        return instructionalObjectiveModel;
+    	//1、找原始的教学目标数据
+    	String initalId = null;
+		String initalSql = "SELECT nd.identifier from resource_relations rr,ndresource nd "
+				+ "where nd.primary_category='instructionalobjectives' and rr.res_type='instructionalobjectives' "
+				+ "and rr.resource_target_type='instructionalobjectives' and rr.target = '"+instructionalObjectiveModel.getIdentifier()+"' "
+				+ "and rr.source_uuid = nd.identifier and nd.enable = 1";
+		
+		List<Map<String,Object>> list = jt.queryForList(initalSql);
+		if(CollectionUtils.isNotEmpty(list)){
+			Map<String, Object> map = list.get(0);
+			initalId = (String)map.get("identifier");
+		}
+		
+		String status = instructionalObjectiveModel.getLifeCycle().getStatus();
+		if("ONLINE".equals(status)){
+			if(initalId != null){
+				instructionalObjectiveModel.setIdentifier(initalId);
+			}
+	        instructionalObjectiveModel.setTechInfoList(null);
+	        instructionalObjectiveModel = (InstructionalObjectiveModel) ndResourceService.update(ResourceNdCode.instructionalobjectives.toString(),
+	                                                                                             instructionalObjectiveModel);
+	        instructionalObjectiveModel.setPreview(null);
+	        instructionalObjectiveModel.setEducationInfo(null);
+
+	        return instructionalObjectiveModel;
+		}else{
+			String oldId = instructionalObjectiveModel.getIdentifier();
+			if(initalId != null){
+				oldId = initalId;
+			}
+			
+			String newId = UUID.randomUUID().toString();
+			instructionalObjectiveModel.setIdentifier(newId);
+			
+			instructionalObjectiveModel = (InstructionalObjectiveModel) ndResourceService.create(ResourceNdCode.instructionalobjectives.toString(),
+                     instructionalObjectiveModel);
+			instructionalObjectiveModel.setPreview(null);
+			instructionalObjectiveModel.setEducationInfo(null);
+			
+			
+			
+			List<EducationRelationModel> educationRelationModels = new ArrayList<EducationRelationModel>();
+			EducationRelationModel erm = new EducationRelationModel();
+			erm.setIdentifier(UUID.randomUUID().toString());
+//			erm.setOrderNum(5000);
+			erm.setResType("instructionalobjectives");
+			erm.setResourceTargetType("instructionalobjectives");
+			erm.setSource(oldId);
+			erm.setTarget(newId);
+			erm.setLabel("版本迭代");
+			erm.setRelationType("ASSOCIATE");
+			EducationRelationLifeCycleModel lc = new EducationRelationLifeCycleModel();
+			lc.setEnable(true);
+			lc.setStatus("CREATED");
+			erm.setLifeCycle(lc);
+			educationRelationModels.add(erm);
+			educationRelationService.createRelation(educationRelationModels, false);
+			
+			return instructionalObjectiveModel;
+			
+		}
     }
 
 }
