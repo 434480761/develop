c0523e31579d10770cd2f8f000a23bed97387978
168956 <lr0609>
2016-06-22 17:33:47 +0800

    添加字形结构分类维度

--- src/main/java/nd/esp/service/lifecycle/utils/category/NdCodePattern.java
+++ src/main/java/nd/esp/service/lifecycle/utils/category/NdCodePattern.java
@@ -322,6 +322,14 @@ public enum NdCodePattern {
             map.put(17,3);
             map.put(20,3);
         }
+    },
+
+    WT("WT|WT[0-9]{2}|WT[0-9]{5}", 7, 2 , ""){
+        @Override
+        void setUp(Map<Integer, Integer> map) {
+            map.put(2,2);
+            map.put(4,3);
+        }
     };
     // 分类维度ndCode的长度(现在所有的分类维度编码长度都是2)
     public static final int CATEGORY_LENGTH = 2;
