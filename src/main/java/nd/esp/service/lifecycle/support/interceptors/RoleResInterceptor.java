5d4814f9759b9b3e903aa0d5b22fd17e8d291dc9
lianggz <lianggz@ND-PC>
2016-07-02 13:42:06 +0800

    添加DAO层单元测试。。。

--- src/main/java/nd/esp/service/lifecycle/support/interceptors/RoleResInterceptor.java
+++ src/main/java/nd/esp/service/lifecycle/support/interceptors/RoleResInterceptor.java
@@ -1,17 +1,21 @@
 package nd.esp.service.lifecycle.support.interceptors;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.ImmutableList;
-import com.nd.gaea.rest.security.authens.UserCenterRoleDetails;
-import com.nd.gaea.rest.security.authens.UserInfo;
-import com.nd.gaea.rest.security.services.WafUserDetailsService;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import nd.esp.service.lifecycle.services.usercoveragemapping.v06.UserCoverageMappingService;
 import nd.esp.service.lifecycle.services.userrestypemapping.v06.UserRestypeMappingService;
 import nd.esp.service.lifecycle.support.LifeCircleErrorMessageMapper;
 import nd.esp.service.lifecycle.support.LifeCircleException;
 import nd.esp.service.lifecycle.support.RoleResFilterUrlMap;
 import nd.esp.service.lifecycle.support.uc.UcRoleClient;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,13 +25,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.ImmutableList;
+import com.nd.gaea.rest.security.authens.UserCenterRoleDetails;
+import com.nd.gaea.rest.security.authens.UserInfo;
+import com.nd.gaea.rest.security.services.WafUserDetailsService;
 
 /**
  * <p>Title: RoleResInterceptor         </p>
@@ -60,7 +63,7 @@ public class RoleResInterceptor implements HandlerInterceptor {
         // 过滤无鉴权处理
         if(authentication != null && !"anonymousUser".equals(authentication.getPrincipal().toString())){
             UserInfo userInfo = (UserInfo)authentication.getPrincipal();
-            if(userInfo != null ){
+            if(userInfo != null){
                 UserCenterRoleDetails userCenterRoleDetails = ucRoleClient.getMaxRole(userInfo);
                 if(userCenterRoleDetails != null){
                     // 库管理员
@@ -109,11 +112,10 @@ public class RoleResInterceptor implements HandlerInterceptor {
     private void isCoverageadminMatcher(HttpServletRequest request, String userId) throws IOException {
 	    for (Map.Entry<String, String> entry : RoleResFilterUrlMap.getCoverageAdminMap().entrySet()) {
             Pattern pattern = Pattern.compile(entry.getKey(), Pattern.CASE_INSENSITIVE);
-            String method = request.getMethod();
-            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + method);
+            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + request.getMethod());
             if(matcher.find()){
                 // 进行coverage权限验证
-                this.isCoverageMatch(method, userId, request);
+                this.isCoverageMatch(request.getMethod(), userId, request);
                 break;
             }
         }
@@ -129,13 +131,12 @@ public class RoleResInterceptor implements HandlerInterceptor {
     private void isRescreatorMatcher(HttpServletRequest request, String userId) throws IOException{
         for (Map.Entry<String, String> entry : RoleResFilterUrlMap.getResCreatorMap().entrySet()) {
             Pattern pattern = Pattern.compile(entry.getKey(), Pattern.CASE_INSENSITIVE);
-            String method = request.getMethod();
-            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + method);
+            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + request.getMethod());
             if(matcher.find()){
                 // 进行resType权限验证
                 this.isResTypeMatch(matcher.group(1), userId);
                 // 进行coverage权限验证
-                this.isCoverageMatch(method, userId, request);
+                this.isCoverageMatch(request.getMethod(), userId, request);
                 break;
             }
         }
@@ -151,13 +152,12 @@ public class RoleResInterceptor implements HandlerInterceptor {
     private void isResconsumerMatcher(HttpServletRequest request, String userId) throws IOException{
         for (Map.Entry<String, String> entry : RoleResFilterUrlMap.getResConsumerMap().entrySet()) {
             Pattern pattern = Pattern.compile(entry.getKey(), Pattern.CASE_INSENSITIVE);
-            String method = request.getMethod();
-            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + method);
+            Matcher matcher = pattern.matcher(request.getRequestURI() + "/" + request.getMethod());
             if(matcher.find()){
                 // 进行resType权限验证
                 this.isResTypeMatch(matcher.group(1), userId);
                 // 进行coverage权限验证
-                this.isCoverageMatch(method, userId, request);
+                this.isCoverageMatch(request.getMethod(), userId, request);
                 break;
             }
         }
@@ -173,7 +173,7 @@ public class RoleResInterceptor implements HandlerInterceptor {
         if(StringUtils.isNotBlank(resType)){
             // 获取当前用户允许访问的resType列表
             List<String> userRestypelList = this.userRestypeMappingService.findUserRestypeList(userId);
-            // resType如果不存在列表中，表示没有权限访问。 报错
+            // resType如果不存在列表中，表示没有权限访问报错
             if( !userRestypelList.contains(resType) ){
                 throw new LifeCircleException(HttpStatus.FORBIDDEN,
                         LifeCircleErrorMessageMapper.Forbidden.getCode(), LifeCircleErrorMessageMapper.Forbidden.getMessage());
@@ -190,17 +190,18 @@ public class RoleResInterceptor implements HandlerInterceptor {
      * @author lianggz
      */
 	private void isCoverageMatch(String method, String userId, HttpServletRequest request) throws IOException{
-        //post,put,delete请求，coverages[i].coverages[i].target值
-        if(ImmutableList.<String>of("POST","PUT","DELETE").contains(method)){
+        //post, put, delete请求，coverages[i].target值
+        if(ImmutableList.<String>of("POST", "PUT", "DELETE").contains(method)){
+            // 从Request中获取请求的Body
             RequestWrapper requestWrapper = new RequestWrapper((HttpServletRequest) request);
             String body = requestWrapper.getBody();
             JSONObject json = JSONObject.parseObject(body);
             JSONArray coverages = json.getJSONArray("coverages");
             // 如果coverages不空的情况下，进行判断
             if(coverages != null && coverages.size() > 0){
+                // 获取用户所拥有的公私有库列表
                 List<String> userCoverageList = this.userCoverageMappingService.findUserCoverageList(userId);
-                Integer num = coverages.size();
-                for(int i=0; i< num; i++){
+                for(int i=0, num = coverages.size(); i < num; i++){
                     JSONObject coverage = coverages.getJSONObject(i);
                     if(coverage != null && coverage.size() >0){
                         // 获取target_type
@@ -209,7 +210,7 @@ public class RoleResInterceptor implements HandlerInterceptor {
                         String target = coverage.getString("target");
                         // 获取coverageStr
                         String coverageStr  = targetType + "/" + target;
-                        // 获取用户的覆盖范围列表,如果不存在, 则没有权限 报错
+                        // 获取用户的覆盖范围列表, 如果不存在, 则没有权限 报错
                         if(!userCoverageList.contains(coverageStr)){
                             throw new LifeCircleException(HttpStatus.FORBIDDEN,
                                     LifeCircleErrorMessageMapper.Forbidden.getCode(), LifeCircleErrorMessageMapper.Forbidden.getMessage());
@@ -218,8 +219,9 @@ public class RoleResInterceptor implements HandlerInterceptor {
                 }
             }
         }
-        //get请求 获取coverage参数值
+        // get请求，获取coverage参数值
         else if(ImmutableList.<String>of("GET").contains(method)){
+            // 从Request中获取请求的parameter
             String coverage = request.getParameter("coverage");
             // 如果coverage不空的情况下，进行判断
             if(StringUtils.isNotBlank(coverage)){
