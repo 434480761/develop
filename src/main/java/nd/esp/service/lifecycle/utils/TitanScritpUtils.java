6ef1a87a5419a6676b4b2a863c5632b911738162
168956 <lr0609>
2016-07-15 10:33:59 +0800

    添加以资源为中心导入数据的相关脚本

--- src/main/java/nd/esp/service/lifecycle/utils/TitanScritpUtils.java
+++ src/main/java/nd/esp/service/lifecycle/utils/TitanScritpUtils.java
@@ -201,6 +201,259 @@ public class TitanScritpUtils {
         return new Properties();
     }
 
+    public static Map<String, Object> buildScript(Education education, List<ResCoverage> coverageList, List<ResourceCategory> categoryList, List<TechInfo> techInfoList){
+        if(education == null){
+            return null;
+        }
+
+        StringBuffer script = new StringBuffer("educationId = graph.addVertex(T.label, education");
+        buildEducationScript(script,education);
+
+        if(CollectionUtils.isNotEmpty(coverageList)){
+            buildCoverageScript(script, coverageList);
+        }
+
+        if(CollectionUtils.isNotEmpty(categoryList)){
+            buildCategoryScript(script, categoryList);
+        }
+
+        if(CollectionUtils.isNotEmpty(techInfoList)){
+            buildTechInfoScript(script, techInfoList);
+        }
+
+        if(CollectionUtils.isNotEmpty(categoryList) ||CollectionUtils.isNotEmpty(coverageList)){
+            buildSearchPropertyScript(script,categoryList,coverageList,education);
+        }
+        return null;
+
+    }
+
+    private static Map<String, Object> buildEducationScript(StringBuffer script, Education education){
+        Map<String, Object> graphParams = getParam4NotNull(education);
+        String suffix = "_edu";
+        Map<String, Object> result = new HashMap<>();
+
+        for (String key : graphParams.keySet()) {
+            script.append(", '").append(key).append("', ").append(key);
+            result.put(key+suffix, graphParams.get(key));
+        }
+        script.append(").id();");
+        return result;
+    }
+
+
+    private static Map<String , Object> buildTechInfoScript(StringBuffer script, List<TechInfo> techInfos){
+        if(CollectionUtils.isEmpty(techInfos)){
+            return new HashMap<>();
+        }
+
+        Map<String, Object> resultParam = new HashMap<>();
+        int orderNumber = 0;
+        StringBuffer techInfoScript = null;
+        for(TechInfo techInfo : techInfos){
+            String suffix = "_ti"+orderNumber;
+            Map<String, Object> techInfoParam = getParam4NotNull(techInfo);
+            techInfoScript  = new StringBuffer("techinfo").append(suffix).append(" = graph.addVertex(T.label, techInfoLabel");
+            for (String key : techInfoParam.keySet()) {
+                techInfoScript.append(", '").append(key).append("', ").append(key).append(suffix);
+                resultParam.put(key + suffix, techInfoParam.get(key));
+            }
+            techInfoScript.append(").id();");
+            techInfoScript.append("g.V(education).next().addEdge('has_tech_info',techinfo ,'identifier',edgeIdentifier").append(suffix).append(")");
+            resultParam.put("edgeIdentifier"+suffix, techInfo.getIdentifier());
+
+            script.append(techInfoScript);
+            orderNumber ++;
+        }
+        return resultParam;
+    }
+
+    private static Map<String , Object> buildCategoryScript(StringBuffer script, List<ResourceCategory> categories){
+        if(CollectionUtils.isEmpty(categories)){
+            return new HashMap<>();
+        }
+
+        Map<String, Object> resultParam = new HashMap<>();
+        Set<String> categoryPathSet = new HashSet<>();
+        int orderNumber = 0;
+        StringBuffer categorieScript = null;
+
+        for(ResourceCategory category : categories){
+            String suffix = "_cg"+orderNumber;
+            String categoryNode = "categoryNode"+suffix;
+            String taxoncodeName = "taxoncode"+suffix;
+            String edgeIdentifierName = "edgeIdentifier" + suffix;
+            Map<String, Object> techInfoParam = getParam4NotNull(category);
+            categorieScript = new StringBuffer(categoryNode+"=g.V().hasLabel('category_code').has('cg_taxoncode',"+taxoncodeName+").id();");
+            StringBuilder addNodeScript = new StringBuilder(categoryNode+"=graph.addVertex(T.label,'category_code'");
+            Map<String, Object> categoryNodeParam = getParam4NotNull(category);
+            for (String key : categoryNodeParam.keySet()) {
+                addNodeScript.append(", '").append(key).append("', ").append(key).append(suffix);
+                resultParam.put(key + suffix, techInfoParam.get(key));
+            }
+
+            String ifScript = "if(!"+categoryNode+".iterator().hasNext()){"+addNodeScript.toString()+"};";
+
+            StringBuilder addEdgeScript = new StringBuilder("g.V(education).next().addEdge('has_category_code',g.V("+categoryNode+").next(),'identifier',"+edgeIdentifierName+");");
+
+            categorieScript.append(ifScript).append(addEdgeScript);
+
+            resultParam.put(taxoncodeName, category.getTaxonname());
+            resultParam.put(edgeIdentifierName, category.getIdentifier());
+
+            script.append(categorieScript);
+
+            if(StringUtils.isNotEmpty(category.getTaxonpath())){
+                categoryPathSet.add(category.getTaxonpath());
+            }
+            orderNumber ++ ;
+        }
+
+        orderNumber = 0;
+        for (String path : categoryPathSet){
+            String suffix = "_cgp"+orderNumber;
+            String categoryPathNode = "categoryNode"+suffix;
+            String taxonpathName = "taxonpath"+suffix;
+            StringBuffer categroyPath = new StringBuffer(categoryPathNode+"=g.V().has('categories_path','cg_taxonpath',"+taxonpathName+").id()");
+
+            String addNodeScript = categoryPathNode+"=graph.addVertex(T.label,'categories_path','cg_taxonpath',"+taxonpathName+").id();";
+            String ifScript = "if(!"+categoryPathNode+".iterator().hasNext()){"+addNodeScript+"};";
+
+            StringBuilder addEdgeScript = new StringBuilder("g.V(education).next().addEdge('has_categories_path',g.V("+categoryPathNode+").next());");
+            categroyPath.append(ifScript).append(addEdgeScript);
+
+            script.append(categroyPath);
+
+            resultParam.put(taxonpathName, path);
+            orderNumber ++ ;
+        }
+
+
+        return resultParam;
+    }
+
+    private static Map<String, Object> buildCoverageScript(StringBuffer script, List<ResCoverage> coverages){
+        if(CollectionUtils.isEmpty(coverages)){
+            return new HashMap<>();
+        }
+
+        Map<String, Object> resultParam = new HashMap<>();
+        int orderNumber = 0;
+        StringBuffer coverageScript = null;
+        for(ResCoverage coverage : coverages){
+            String suffix = "_cov"+orderNumber;
+            String targetTypeName = "target_type" +suffix;
+            String targetName = "target" +suffix;
+            String strategyName = "strategy" +suffix;
+            String coverageNodeName = "coverageNode"+suffix;
+            String edgeIdentifierName = "edgeIdentifier"+suffix;
+            coverageScript = new StringBuffer(coverageNodeName+"=g.V().hasLabel('coverage').has('target_type',"+targetTypeName+").has('target',"+targetName+").has('strategy',"+strategyName+").id();");
+            StringBuilder addCoverageNodeScript = new StringBuilder(coverageNodeName+ "= graph.addVertex(T.label,'coverage','target_type',"+targetTypeName+",'strategy',"+strategyName+",'target',"+targetName+").id();");
+            String ifScript = "if(!"+coverageNodeName+".iterator().hasNext()){"+addCoverageNodeScript+"};";
+            String addEdgeScript = "g.V(education).next().addEdge('has_coverage',g.V(coverageNodeId).next(),'identifier',"+edgeIdentifierName+").id();";
+
+            coverageScript.append(ifScript).append(addEdgeScript);
+
+            script.append(coverageScript);
+            resultParam.put(targetTypeName, coverage.getTargetType());
+            resultParam.put(targetName, coverage.getTarget());
+            resultParam.put(strategyName, coverage.getStrategy());
+            resultParam.put(edgeIdentifierName, coverage.getIdentifier());
+            orderNumber ++;
+        }
+        return resultParam;
+    }
+
+    private static Map<String, Object> buildRelationScript(StringBuffer script, List<ResourceRelation> resourceRelations){
+        int orderNumber = 0;
+        Map<String, Object> result = new HashMap<>();
+        for(ResourceRelation resourceRelation : resourceRelations){
+            String suffix = "_rr"+orderNumber;
+            String sourcePrimaryCategoryName = "source_primaryCategory"+suffix;
+            String sourceIdentifierName = "source_identifier"+suffix;
+            String targetPrimaryCategoryName = "target_primaryCategory"+suffix;
+            String targetIdentifierName = "target_identifier"+suffix;
+            String edgeIdentifierName = "edgeIdentifier"+suffix;
+
+            StringBuffer scriptBuffer = new StringBuffer(
+                    "g.V().hasLabel("+sourcePrimaryCategoryName+").has('identifier',"+sourceIdentifierName+").next()" +
+                            ".addEdge('has_relation'," +
+                            "g.V().hasLabel("+targetPrimaryCategoryName+").has('identifier',"+targetIdentifierName+").next(),'identifier',"+edgeIdentifierName);
+
+            Map<String, Object> createRelationParams = getParam4NotNull(resourceRelation);
+            for (String key : createRelationParams.keySet()) {
+                scriptBuffer.append(", '").append(key).append("', ").append(key).append(suffix);
+                result.put(key + suffix, createRelationParams.get(key));
+            }
+
+            scriptBuffer.append(").id()");
+
+            createRelationParams.put("sourcePrimaryCategoryName", resourceRelation.getResType());
+            createRelationParams.put("sourceIdentifierName", resourceRelation.getSourceUuid());
+            createRelationParams.put("targetPrimaryCategoryName", resourceRelation.getResourceTargetType());
+            createRelationParams.put("targetIdentifierName", resourceRelation.getTarget());
+            createRelationParams.put("edgeIdentifierName", resourceRelation.getIdentifier());
+
+            script.append(scriptBuffer);
+        }
+
+        return result;
+    }
+
+    private static Map<String, Object> buildSearchPropertyScript(StringBuffer script,List<ResourceCategory> categories, List<ResCoverage> coverages ,Education education){
+        Set<String> resCoverages = new HashSet<>() ;
+        Set<String> categoryCodes = new HashSet<>();
+        Set<String> paths = new HashSet<>();
+        if(CollectionUtils.isNotEmpty(coverages)){
+            for(ResCoverage resCoverage : coverages){
+                String setValue4 = resCoverage.getTargetType()+"/"+resCoverage.getTarget()+"/"+resCoverage.getStrategy()+"/"+education.getStatus();
+                String setValue3 = resCoverage.getTargetType()+"/"+resCoverage.getTarget()+"//"+education.getStatus();
+                String setValue2 = resCoverage.getTargetType()+"/"+resCoverage.getTarget()+"/"+resCoverage.getStrategy()+"/";
+                String setValue1 = resCoverage.getTargetType()+"/"+resCoverage.getTarget()+"//";
+                resCoverages.add(setValue1);
+                resCoverages.add(setValue2);
+                resCoverages.add(setValue3);
+                resCoverages.add(setValue4);
+            }
+        }
+
+        if(CollectionUtils.isNotEmpty(categories)){
+            for(ResourceCategory category : categories){
+                if(StringUtils.isNotEmpty(category.getTaxonpath())){
+                    paths.add(category.getTaxonpath());
+                }
+                if(StringUtils.isNotEmpty(category.getTaxoncode())){
+                    categoryCodes.add(category.getTaxoncode());
+                }
+
+            }
+        }
+
+        StringBuffer searchPropertyScript = new StringBuffer("g.V(education)");
+        Map<String, Object> param = new HashMap<>();
+
+        addSetProperty("search_coverage",resCoverages,searchPropertyScript,param);
+        addSetProperty("search_code",categoryCodes,searchPropertyScript,param);
+        addSetProperty("search_path",paths,searchPropertyScript,param);
+
+        script.append(searchPropertyScript);
+        return param;
+    }
+
+    private static void addSetProperty(String fieldName,
+                                Set<String> values ,StringBuffer script ,Map<String, Object> param) {
+        if(values == null || values.size() == 0){
+            return;
+        }
+        int index = 0;
+        for (String value : values){
+            String paramKey = fieldName+index;
+            index ++ ;
+            script.append(".property(set,'").append(fieldName).append("',").append(paramKey).append(")");
+            param.put(paramKey , value);
+        }
+    }
+
     public static  void main(String[] args){
         StringBuffer scriptBuffer = new StringBuffer();
         String name = "name";
