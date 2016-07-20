79aa80fcd194258c328661b4e78920acfc57e6af
168956 <lr0609>
2016-07-20 14:31:50 +0800

    更改titan数据同步轮训时间

--- src/main/java/nd/esp/service/lifecycle/daos/titan/TitanSyncTimerTask.java
+++ src/main/java/nd/esp/service/lifecycle/daos/titan/TitanSyncTimerTask.java
@@ -23,7 +23,7 @@ import java.util.*;
 @Component
 public class TitanSyncTimerTask {
     private final static Logger LOG = LoggerFactory.getLogger(TitanSyncTimerTask.class);
-    public static int MAX_REPORT_TIMES = 20;
+    public static int MAX_REPORT_TIMES = 10;
     public static boolean LOCKED = false;
 
     @Autowired
