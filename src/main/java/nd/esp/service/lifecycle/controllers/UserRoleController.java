80c77ef2cc198d49b77c85d7e7b16e3361070917
144231 <lan520>
2016-07-21 14:14:54 +0800

    阻断问题跟测试用例覆盖率问题修复

--- src/main/java/nd/esp/service/lifecycle/controllers/UserRoleController.java
+++ src/main/java/nd/esp/service/lifecycle/controllers/UserRoleController.java
@@ -237,7 +237,7 @@ public class UserRoleController {
 		//返回内容
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("limit", limit);
-		result.put("items", userRoleViewModelArrayList.toArray());
+		result.put("items", userRoleViewModelArrayList == null ? null :userCoverageMappingModelList.toArray());
 		return result;
 	}
 
