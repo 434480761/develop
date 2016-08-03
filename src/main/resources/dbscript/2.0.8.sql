b0c0644a30cc7336ac6048e81832659d3bab91fb
xiezy <xiezy@101.com>
2016-08-03 11:41:54 +0800

    2.0.8sql

--- src/main/resources/dbscript/2.0.8.sql
+++ src/main/resources/dbscript/2.0.8.sql
@@ -1,1 +1,6 @@
 CREATE TABLE `recommended_resources` ( `recommended_id` CHAR(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , PRIMARY KEY (`recommended_id`) ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT = '推荐资源列表';
+
+INSERT INTO `recommended_resources` (`recommended_id`) VALUES
+('0152af89-4555-4217-80a5-366f91044c7c'),
+('79b85fb2-20de-4063-ad53-32dada2ba5be'),
+('d698d8fd-7737-45ff-be3b-0777f530d24e');
