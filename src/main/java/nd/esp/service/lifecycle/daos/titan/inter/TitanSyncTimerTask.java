c7e1751cfb729e061427a99481a99e6d14371b88
168956 <lr0609>
2016-08-04 11:30:56 +0800

    删掉和并错误的类

--- src/main/java/nd/esp/service/lifecycle/daos/titan/inter/TitanSyncTimerTask.java
+++
@@ -1,8 +0,0 @@
-package nd.esp.service.lifecycle.daos.titan.inter;
-
-/**
- * Created by liuran on 2016/7/11.
- */
-public interface TitanSyncTimerTask {
-    void sync();
-}
--- src/main/java/nd/esp/service/lifecycle/support/busi/titan/TitanErrorReason.java
+++
@@ -1,8 +0,0 @@
-package nd.esp.service.lifecycle.support.busi.titan;
-
-/**
- * Created by liuran on 2016/7/5.
- */
-public enum TitanErrorReason {
-    TECH_INFO ,RELATION, CATEGORY,COVERAGE
-}
