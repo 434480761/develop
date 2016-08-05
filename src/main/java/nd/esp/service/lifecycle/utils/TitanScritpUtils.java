673b1633e29d6d417f3f5ee2f3dbf966354a803e
168956 <lr0609>
2016-08-05 14:35:58 +0800

    获取详情脚本增加对知识点扩展属性的支持

--- src/main/java/nd/esp/service/lifecycle/utils/TitanScritpUtils.java
+++ src/main/java/nd/esp/service/lifecycle/utils/TitanScritpUtils.java
@@ -591,9 +591,9 @@ public class TitanScritpUtils {
         if(!isAll){
             scriptBuilder.append(".has('lc_enable',true)");
         }
-
         params.put("primary_category", primaryCategory);
-        scriptBuilder.append(TitanUtils.generateScriptForInclude(includeList,null));
+
+        scriptBuilder.append(TitanUtils.generateScriptForInclude(includeList,primaryCategory));
         scriptBuilder.append(".valueMap();");
 
         Map<KeyWords, Object> result = new HashMap<>();
