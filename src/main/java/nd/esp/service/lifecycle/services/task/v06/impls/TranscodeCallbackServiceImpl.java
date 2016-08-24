f699b69d3862968b844d20e2bf66f77ee6bc479d
qiling <422052427@qq.com>
2016-08-24 14:56:22 +0800

    swf视频转码及ndpx课件包转码..

--- src/main/java/nd/esp/service/lifecycle/services/task/v06/impls/TranscodeCallbackServiceImpl.java
+++ src/main/java/nd/esp/service/lifecycle/services/task/v06/impls/TranscodeCallbackServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.Map.Entry;
 import java.util.UUID;
 
 import org.apache.log4j.MDC;
+import org.hibernate.mapping.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,7 +53,7 @@ public class TranscodeCallbackServiceImpl implements TranscodeCallbackService {
     
     private static final String TECH_INFO_SOURCE_KEY="source";
     private static final String TECH_INFO_HREF_KEY="href";
-    private static final String [] TECH_INFO_HREF_KEYS_ARR={"href","href-360p","href-480p","href-720p","href-360p-ogv","href-480p-ogv","href-720p-ogv","href-1080p-ogv"};
+    private static final String [] TECH_INFO_HREF_KEYS_ARR={"href","href-360p","href-480p","href-720p","href-360p-ogv","href-480p-ogv","href-720p-ogv","href-ogv"};
     private static final List<String> TECH_INFO_HREF_KEYS = Arrays.asList(TECH_INFO_HREF_KEYS_ARR);
     private static final String VIDEO_FORMAT_TARGET="mp4";
     private static final String VIDEO_THEORA_FORMAT="ogv";
@@ -221,16 +222,20 @@ public class TranscodeCallbackServiceImpl implements TranscodeCallbackService {
             Map<String, Object> metadataMap = ObjectUtils.fromJson(metadata, Map.class);
             // List<TechnologyRequirementModel> requirementModels = new ArrayList<TechnologyRequirementModel>();
             List<TechnologyRequirementModel> requirementModels = techInfo.getRequirements();
-            if(requirementModels == null){
+//            if(requirementModels == null){
                 requirementModels = new ArrayList<TechnologyRequirementModel>();
                 techInfo.setRequirements(requirementModels);
-            }
+//            }
             for (Entry<String, Object> techData : metadataMap.entrySet()) {
                 TechnologyRequirementModel technologyRequirementModel = new TechnologyRequirementModel();
                 technologyRequirementModel.setIdentifier(UUID.randomUUID().toString());
                 technologyRequirementModel.setType(RequirementType.QUOTA.toString());
                 technologyRequirementModel.setName(techData.getKey());
-                technologyRequirementModel.setValue(String.valueOf(techData.getValue()));
+                if(technologyRequirementModel.getName().equals("Video") || technologyRequirementModel.getName().equals("Audio")) {
+                    technologyRequirementModel.setValue(ObjectUtils.toJson(techData.getValue()));
+                } else {
+                    technologyRequirementModel.setValue(String.valueOf(techData.getValue()));
+                }
                 requirementModels.add(technologyRequirementModel);
             }
             // techInfo.setRequirements(requirementModels);
