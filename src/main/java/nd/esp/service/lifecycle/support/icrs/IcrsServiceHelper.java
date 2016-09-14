26630842f61ab69f83e942312e850e639a750341
xiezy <xiezy@101.com>
2016-09-14 15:34:50 +0800

    智慧课堂的数据统计 暂时提交

--- src/main/java/nd/esp/service/lifecycle/support/icrs/IcrsServiceHelper.java
+++ src/main/java/nd/esp/service/lifecycle/support/icrs/IcrsServiceHelper.java
@@ -5,16 +5,25 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+import com.nd.gaea.client.http.WafHttpClient;
+import com.nd.gaea.client.http.WafSecurityHttpClient;
+
+import nd.esp.service.lifecycle.app.LifeCircleApplicationInitializer;
 import nd.esp.service.lifecycle.educommon.services.impl.CommonServiceHelper;
 import nd.esp.service.lifecycle.repository.common.IndexSourceType;
+import nd.esp.service.lifecycle.repository.exception.EspStoreException;
 import nd.esp.service.lifecycle.repository.model.icrs.IcrsResource;
 import nd.esp.service.lifecycle.repository.sdk.icrs.IcrsResourceRepository;
+import nd.esp.service.lifecycle.support.LifeCircleErrorMessageMapper;
+import nd.esp.service.lifecycle.support.LifeCircleException;
 import nd.esp.service.lifecycle.support.busi.CommonHelper;
 import nd.esp.service.lifecycle.utils.CollectionUtils;
 
@@ -77,10 +86,36 @@ public class IcrsServiceHelper {
 			});
 			
 			if(CollectionUtils.isNotEmpty(list)){
-				List<IcrsResource> syncList = new ArrayList<IcrsResource>();
-				for(SyncIcrsModel model : list){
-					IcrsResource searchExample = new IcrsResource();
-					searchExample.setResUuid(model.getIdentifier());
+				try {
+					List<IcrsResource> syncList = new ArrayList<IcrsResource>();
+					for(SyncIcrsModel model : list){
+						//先查询是否同步过
+						IcrsResource searchExample = new IcrsResource();
+						searchExample.setResUuid(model.getIdentifier());
+						List<IcrsResource> existList = icrsResourceRepository.getAllByExample(searchExample);
+						
+						//如果已同步过，先删除旧数据
+						if(CollectionUtils.isNotEmpty(existList)){
+							List<String> deleteIds = new ArrayList<String>();
+							for(IcrsResource ir : existList){
+								deleteIds.add(ir.getIdentifier());
+							}
+							
+							if(CollectionUtils.isNotEmpty(deleteIds)){
+								icrsResourceRepository.batchDel(deleteIds);
+							}
+						}
+						
+						//新增同步记录
+						//1.查询用户姓名,调用UC接口
+						String userName = getUserName(model.getTarget());
+						//2.获取对应学校id,调用Admin接口
+						
+					}
+				} catch (Exception e) {
+					throw new LifeCircleException(HttpStatus.INTERNAL_SERVER_ERROR,
+	                        LifeCircleErrorMessageMapper.StoreSdkFail.getCode(),
+	                        e.getLocalizedMessage());
 				}
 			}else{
 				break;
@@ -102,6 +137,43 @@ public class IcrsServiceHelper {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) - 1);
 		return calendar.getTimeInMillis();
+	}
+	
+	/**
+	 * 获取用户姓名
+	 * @author xiezy
+	 * @date 2016年9月14日
+	 * @param userid
+	 * @return
+	 * @throws Exception
+	 */
+	private String getUserName(String userid) throws Exception {
+		WafSecurityHttpClient wafSecurityHttpClient = new WafSecurityHttpClient();
+		String url = LifeCircleApplicationInitializer.properties.getProperty("waf.uc.uri") + "users/" + userid;
+		@SuppressWarnings("deprecation")
+		Map<String, Object> userInfo = wafSecurityHttpClient.get(url, Map.class);
+		
+		if(CollectionUtils.isEmpty(userInfo)){
+			return "";
+		}
+		
+		return (String) userInfo.get("nick_name");
+	}
+	
+	/**
+	 * 根据教师id获取学校id
+	 * @author xiezy
+	 * @date 2016年9月14日
+	 * @param teacherId
+	 * @return
+	 */
+	private String getSchoolId(String teacherId){
+		WafSecurityHttpClient wafSecurityHttpClient = new WafSecurityHttpClient();
+		String url = LifeCircleApplicationInitializer.properties.getProperty("admin.uri") 
+				+ "v06/schools/" + teacherId;
+		
+		
+		return teacherId;
 	}
 	
 	/**
