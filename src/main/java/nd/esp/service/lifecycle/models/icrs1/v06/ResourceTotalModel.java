6fba09f8841fd7546855409a9f92ff906677b27f
yzc <1799281706@qq.com>
2016-09-18 09:20:13 +0800

    修改

--- src/main/java/nd/esp/service/lifecycle/models/icrs1/v06/ResourceTotalModel.java
+++ src/main/java/nd/esp/service/lifecycle/models/icrs1/v06/ResourceTotalModel.java
@@ -1,21 +1,24 @@
 package nd.esp.service.lifecycle.models.icrs1.v06;
 
 public class ResourceTotalModel {
-	
-	private String  resType;
+
+	private String resType;
 	private int resTotal;
+
 	public String getResType() {
 		return resType;
 	}
+
 	public void setResType(String resType) {
 		this.resType = resType;
 	}
+
 	public int getResTotal() {
 		return resTotal;
 	}
+
 	public void setResTotal(int resTotal) {
 		this.resTotal = resTotal;
 	}
-
 
 }
