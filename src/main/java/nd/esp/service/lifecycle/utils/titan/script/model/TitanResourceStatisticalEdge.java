afe191ff56b98a0ec059c6c1f8b0042e871e8636
168956 <lr0609>
2016-09-18 20:54:15 +0800

    增加统计属性

---
+++ src/main/java/nd/esp/service/lifecycle/utils/titan/script/model/TitanResourceStatisticalEdge.java
@@ -0,0 +1,105 @@
+package nd.esp.service.lifecycle.utils.titan.script.model;
+
+import nd.esp.service.lifecycle.utils.titan.script.annotation.TitanCompositeKey;
+import nd.esp.service.lifecycle.utils.titan.script.annotation.TitanEdgeResourceKey;
+import nd.esp.service.lifecycle.utils.titan.script.annotation.TitanEdgeTargetKey;
+import nd.esp.service.lifecycle.utils.titan.script.annotation.TitanField;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Administrator on 2016/9/18.
+ */
+public class TitanResourceStatisticalEdge {
+    @TitanEdgeTargetKey(target = "identifier")
+    @TitanCompositeKey
+    @TitanField(name = "identifier")
+    private String identifier;
+
+    @TitanField(name = "sta_title")
+    private String title;
+
+    @TitanField(name = "sta_key_title")
+    private String keyTitle;
+
+    @TitanField(name = "sta_key_value")
+    private Double keyValue;
+
+    @TitanField(name = "sta_update_time")
+    private Timestamp updateTime;
+
+    @TitanField(name = "sta_data_from")
+    private String dataFrom;
+
+    @TitanEdgeResourceKey(source= "identifier")
+    @TitanField(name="sta_resource")
+    private String resource;
+
+    @TitanEdgeResourceKey(source = "primary_category")
+    @TitanField(name = "sta_res_type")
+    private String resType;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getKeyTitle() {
+        return keyTitle;
+    }
+
+    public void setKeyTitle(String keyTitle) {
+        this.keyTitle = keyTitle;
+    }
+
+    public Double getKeyValue() {
+        return keyValue;
+    }
+
+    public void setKeyValue(Double keyValue) {
+        this.keyValue = keyValue;
+    }
+
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getDataFrom() {
+        return dataFrom;
+    }
+
+    public void setDataFrom(String dataFrom) {
+        this.dataFrom = dataFrom;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getResType() {
+        return resType;
+    }
+
+    public void setResType(String resType) {
+        this.resType = resType;
+    }
+}
