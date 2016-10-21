4c624d4619d863ad32da9b10ae5938e3c407f4d5
qiling <422052427@qq.com>
2016-10-21 22:01:07 +0800

    文档转码update; 视频转码使用cs_sdk

--- task-worker-document-transcode/src/main/java/com/nd/esp/task/worker/buss/document_transcode/utils/Txt2htmlUtil.java
+++ task-worker-document-transcode/src/main/java/com/nd/esp/task/worker/buss/document_transcode/utils/Txt2htmlUtil.java
@@ -1,9 +1,11 @@
 package com.nd.esp.task.worker.buss.document_transcode.utils;
 
+import info.monitorenter.cpdetector.io.*;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,7 +15,8 @@ import java.util.regex.Pattern;
 public class Txt2htmlUtil {
     public static void transferTxt2Html(String inputFilePath, String outDirPath) throws IOException {
         File input = new File(inputFilePath);
-        String strIn = FileUtils.readFileToString(input);
+
+        String strIn = FileUtils.readFileToString(input, getEncoding(inputFilePath));
         String strOut = txtToHtml(strIn);
         File outDir = new File(outDirPath);
         if(!outDir.exists()) {
@@ -21,11 +24,38 @@ public class Txt2htmlUtil {
         }
         FileUtils.cleanDirectory(outDir);
 
-        FileUtils.writeStringToFile(new File(outDir, input.getName().replace(".txt", ".html")), strOut);
+        FileUtils.writeStringToFile(new File(outDir, "1.html"), strOut, "utf-8");
+    }
+
+    private static Charset getEncoding(String inputFile) {
+        CodepageDetectorProxy detector =
+        CodepageDetectorProxy.getInstance();
+        /*-------------------------------------------------------------------------
+          ParsingDetector可用于检查HTML、XML等文件或字符流的编码,构造方法中的参数用于
+          指示是否显示探测过程的详细信息，为false不显示。
+        ---------------------------------------------------------------------------*/
+        detector.add(new ParsingDetector(false));
+        /*--------------------------------------------------------------------------
+          JChardetFacade封装了由Mozilla组织提供的JChardet，它可以完成大多数文件的编码
+          测定。所以，一般有了这个探测器就可满足大多数项目的要求，如果你还不放心，可以
+          再多加几个探测器，比如下面的ASCIIDetector、UnicodeDetector等。
+         ---------------------------------------------------------------------------*/
+        detector.add(JChardetFacade.getInstance());
+        //ASCIIDetector用于ASCII编码测定
+        detector.add(ASCIIDetector.getInstance());
+        //UnicodeDetector用于Unicode家族编码的测定
+        detector.add(UnicodeDetector.getInstance());
+        Charset charset = null;
+        File f=new File(inputFile);
+        try {
+            charset = detector.detectCodepage(f.toURI().toURL());
+        } catch (Exception ex) {ex.printStackTrace();}
+        return charset;
     }
 
     public static String txtToHtml(String s) {
         StringBuilder builder = new StringBuilder();
+        builder.append("<meta charset=\"utf-8\" />");
         boolean previousWasASpace = false;
         for (char c : s.toCharArray()) {
             if (c == ' ') {
